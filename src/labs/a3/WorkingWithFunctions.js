62b4d3917227c383b252af559d86f714970da070
Chandresh Lokesha <chandresh@Chandreshs-MacBook-Pro.local>
2023-05-30 12:21:38 -0400

    A3 and Tuiter-react

---
+++ src/labs/a3/WorkingWithFunctions.js
@@ -0,0 +1,18 @@
+import ES5Functions from './es-5-functions';
+import ArrowFunctions from './arrow-functions';
+import ImpliedReturn from './implied-return';
+import FunctionParenthesisAndParameters from './function-parenthesis-and-parameters';
+
+function WorkingWithFunctions() {
+  return (
+    <div>
+      {/* Other components */}
+      <ES5Functions />
+      <ArrowFunctions/>
+      <ImpliedReturn/>
+      <FunctionParenthesisAndParameters/>
+    </div>
+  );
+}
+
+export default WorkingWithFunctions;
---
+++ src/tuiter/explore-screen-old.js
@@ -0,0 +1,13 @@
+import TuitSummaryItem from "./tuit-summary-list/tuit-summary-item";
+import TuitSummaryList from "./tuit-summary-list";
+
+function ExploreScreen() {
+    return (
+    <>
+    <h1>Explore</h1>
+        <TuitSummaryList/>
+    </>
+    
+    );
+   }
+   export default ExploreScreen;
